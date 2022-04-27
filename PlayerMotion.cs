f0cf57a6df4c28ae9cae4322289d8a1fcfaca4d2
Manas Kumar <70679511+Manas1114880@users.noreply.github.com>
2022-04-27 00:06:35 -0700

    Add files via upload

---
+++ PlayerMotion.cs
@@ -0,0 +1,25 @@
+
+using UnityEngine;
+using System.Collections;
+
+public class PlayerMotion : MonoBehaviour
+{
+
+    public float speed;
+
+    public Start(){
+
+    }
+
+
+    public Vector3 Update(){
+
+        speed = 5.0;
+        float xDirection = speed*Input.GetAxis("Horizontal");
+        float zDirection = speed*Input.GetAxis("Vertical");
+
+       Vector3 motion = transform.Translate(xDirection, 0,zDirection);
+
+       return motion;
+    }
+}
