543c604371b39343b2e8260df8619079353f341c
Manas1114880 <1114880@apps.nsd.org>
2022-05-13 09:16:27 -0700

    "I put different values for the motion object"

--- PlayerMotion.cs
+++ PlayerMotion.cs
@@ -17,11 +17,7 @@ public class PlayerMotion : MonoBehaviour
 
     public Vector3 Update(){
 
-        float speed = 5.0;
-        float xDirection = speed*Input.GetAxis("Horizontal");
-        float zDirection = speed*Input.GetAxis("Vertical");
-
-       Vector3 motion = transform.Translate(xDirection, 0,zDirection);
+       Vector3 motion = transform.Translate(1.0, 0,1.0);
 
        return motion;
     }
